96fcdb3fce7b43fe47a66d37a140555c10dcde6a
Kyle Trinh <kyle.trinh94@gmail.com>
2022-03-05 21:10:52 -0800

    test tes

--- web/src/components/Testimonials.js
+++ web/src/components/Testimonials.js
@@ -60,105 +60,107 @@ export default function Testimonials() {
         console.log(data);
         console.log("-------------------------------");
         return (
-          <Box as="section" py="3rem" id="testimonials">
-            <Container>
-              <Text
-                fontFamily={"Playfair Display, serif;"}
-                fontWeight="700"
-                fontSize="1.8rem"
-                mb="0.8rem"
-                color="pinkPrimary"
-                textAlign={"center"}
-              >
-                {data.content?.subHeading}
-              </Text>
-              <SectionHeading>{data.content.heading}</SectionHeading>
-              <Box
-                bg="white"
-                position="relative"
-                _hover={{
-                  "& > .swiper-button-prev": {
-                    opacity: "1",
-                    visibility: "visible",
-                  },
-                  "& > .swiper-button-next": {
-                    opacity: "1",
-                    visibility: "visible",
-                  },
-                }}
-              >
-                <Box className="swiper mySwiper2" w="100%" py="4rem">
-                  <Box className="swiper-wrapper" h="100%">
-                    {data.content.sectionContent.map((review) => (
-                      <Box
-                        className="swiper-slide"
-                        key={review._key}
-                        h="90%"
-                        overflow={"hidden"}
-                        textAlign="center"
-                      >
-                        <VStack
-                          align="center"
-                          justify={"center"}
-                          h="100%"
-                          px="8rem"
+          data.content && (
+            <Box as="section" py="3rem" id="testimonials">
+              <Container>
+                <Text
+                  fontFamily={"Playfair Display, serif;"}
+                  fontWeight="700"
+                  fontSize="1.8rem"
+                  mb="0.8rem"
+                  color="pinkPrimary"
+                  textAlign={"center"}
+                >
+                  {data.content?.subHeading}
+                </Text>
+                <SectionHeading>{data.content.heading}</SectionHeading>
+                <Box
+                  bg="white"
+                  position="relative"
+                  _hover={{
+                    "& > .swiper-button-prev": {
+                      opacity: "1",
+                      visibility: "visible",
+                    },
+                    "& > .swiper-button-next": {
+                      opacity: "1",
+                      visibility: "visible",
+                    },
+                  }}
+                >
+                  <Box className="swiper mySwiper2" w="100%" py="4rem">
+                    <Box className="swiper-wrapper" h="100%">
+                      {data.content.sectionContent.map((review) => (
+                        <Box
+                          className="swiper-slide"
+                          key={review._key}
+                          h="90%"
+                          overflow={"hidden"}
+                          textAlign="center"
                         >
-                          <Image
-                            src={imageUrlFor(buildImageObj(review.profilePic))
-                              .width(400)
-                              .height(400)
-                              .url()}
-                            w="100px"
-                            borderRadius={"50%"}
-                          />
-                          <Text fontSize="1.6rem" textAlign={"center"}>
-                            {review.review}
-                          </Text>
-                          <Text color="pinkPrimary" fontSize="1.8rem">
-                            {review.name} - {review.city}
-                          </Text>
-                        </VStack>
-                      </Box>
-                    ))}
+                          <VStack
+                            align="center"
+                            justify={"center"}
+                            h="100%"
+                            px="8rem"
+                          >
+                            <Image
+                              src={imageUrlFor(buildImageObj(review.profilePic))
+                                .width(400)
+                                .height(400)
+                                .url()}
+                              w="100px"
+                              borderRadius={"50%"}
+                            />
+                            <Text fontSize="1.6rem" textAlign={"center"}>
+                              {review.review}
+                            </Text>
+                            <Text color="pinkPrimary" fontSize="1.8rem">
+                              {review.name} - {review.city}
+                            </Text>
+                          </VStack>
+                        </Box>
+                      ))}
+                    </Box>
+                    <Box
+                      className="swiper-pagination"
+                      __css={{
+                        "& > .swiper-pagination-bullet-active": {
+                          background: "buttonPrimary",
+                        },
+                      }}
+                    />
                   </Box>
                   <Box
-                    className="swiper-pagination"
-                    __css={{
-                      "& > .swiper-pagination-bullet-active": {
-                        background: "buttonPrimary",
-                      },
-                    }}
+                    className="swiper-button-prev"
+                    left="-30px"
+                    _after={{ content: '"←"' }}
+                    color="textPrimary"
+                    opacity={0}
+                    visibility="hidden"
+                    transition="all 0.2s ease-out"
+                    bg="#faefed"
+                    borderRadius={"50%"}
+                    p="4rem 3rem"
+                    _hover={{ color: "pinkPrimary" }}
+                  />
+                  <Box
+                    className="swiper-button-next"
+                    right="-30px"
+                    _after={{ content: '"→"' }}
+                    color="textPrimary"
+                    opacity={0}
+                    visibility="hidden"
+                    transition="all 0.2s ease-out"
+                    bg="#faefed"
+                    borderRadius={"50%"}
+                    p="4rem 3rem"
+                    _hover={{ color: "pinkPrimary" }}
                   />
                 </Box>
-                <Box
-                  className="swiper-button-prev"
-                  left="-30px"
-                  _after={{ content: '"←"' }}
-                  color="textPrimary"
-                  opacity={0}
-                  visibility="hidden"
-                  transition="all 0.2s ease-out"
-                  bg="#faefed"
-                  borderRadius={"50%"}
-                  p="4rem 3rem"
-                  _hover={{ color: "pinkPrimary" }}
-                />
-                <Box
-                  className="swiper-button-next"
-                  right="-30px"
-                  _after={{ content: '"→"' }}
-                  color="textPrimary"
-                  opacity={0}
-                  visibility="hidden"
-                  transition="all 0.2s ease-out"
-                  bg="#faefed"
-                  borderRadius={"50%"}
-                  p="4rem 3rem"
-                  _hover={{ color: "pinkPrimary" }}
-                />
-              </Box>
-            </Container>
-          </Box>
+              </Container>
+            </Box>
+          )
         );
       }}
     />
